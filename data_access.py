19aed642253266644087adbecd29538e47c22c0c
Luiz <150481961+LuizKiko@users.noreply.github.com>
2023-11-22 17:41:46 +0100

    Update data_access.py

--- data_access.py
+++ data_access.py
@@ -1,7 +1,6 @@
-from dotenv import load_dotenv
 import pandas as pd
 
 
-dfSki = pd.read_csv(r"ski\resorts.csv")
-dfSnow = pd.read_csv(r"ski\snow.csv")
-dfCountryCode = pd.read_csv(r"ski\CountryCodes.csv")
+dfSki = pd.read_csv("resorts.csv")
+dfSnow = pd.read_csv("snow.csv")
+dfCountryCode = pd.read_csv("CountryCodes.csv")
