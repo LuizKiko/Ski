64abec45c6dd7f999a2dd0be68dccd9814d3f1e6
Luiz <150481961+LuizKiko@users.noreply.github.com>
2023-11-22 17:38:58 +0100

    Update data_access.py

--- data_access.py
+++ data_access.py
@@ -1,9 +1,6 @@
-import os
 from dotenv import load_dotenv
-import mysql.connector
 import pandas as pd
 
-load_dotenv()
 
 dfSki = pd.read_csv(r"ski\resorts.csv")
 dfSnow = pd.read_csv(r"ski\snow.csv")
