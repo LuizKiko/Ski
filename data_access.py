857008a6c69c4004bea2925e4214ded18376e0c0
Luiz <150481961+LuizKiko@users.noreply.github.com>
2023-11-22 17:31:05 +0100

    Update data_access.py

--- data_access.py
+++ data_access.py
@@ -5,14 +5,6 @@ import pandas as pd
 
 load_dotenv()
 
-dbSki = mysql.connector.connect(
-    host=os.getenv('HOST'),
-    user=os.getenv('USER'),
-    passwd=os.getenv('PASSWD'),
-    database=os.getenv('DATABASE')
-    )
-
-qSki = os.getenv('QSKI')
 dfSki = pd.read_csv(r"ski\resorts.csv")
 dfSnow = pd.read_csv(r"ski\snow.csv")
 dfCountryCode = pd.read_csv(r"ski\CountryCodes.csv")
